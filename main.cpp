68dfd12ece324f60f798fac280666ee837fa15d8
ivan <ivan.sharapov.05@yandex.ru>
2024-11-12 15:22:52 +0300

    For linux

--- main.cpp
+++ main.cpp
@@ -9,26 +9,6 @@
 
 using namespace std;
 
-void scan_set(char t[U + 1]) {
-    char x;
-    int cnt = 0;
-    do {
-        x = (char) getc(stdin);
-        if ('0' <= x && x <= '9') {
-            bool found = false;
-            for (int i = 0; i < cnt; ++i) {
-                if (t[i] == x) {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                t[cnt++] = x;
-            }
-        }
-    } while (x != '\n');
-}
-
 void generatorWithFixedLen(char a[], int len) {
     mt19937 rnd(random_device{}());
     char digits[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
@@ -43,90 +23,55 @@ void generatorWithFixedLen(char a[], int len) {
 }
 
 int main() {
+    freopen("log.txt", "w", stdout);
     char a[U + 1]{}, b[U + 1]{}, c[U + 1]{}, d[U + 1]{};
-    char mode;
-    int ln;
+    for (int ln = 1; ln <= 10; ++ln) {
+        for (int i = 0; i < 1000; ++i) {
+            generatorWithFixedLen(a, ln);
+            generatorWithFixedLen(b, ln);
+            generatorWithFixedLen(c, ln);
+            generatorWithFixedLen(d, ln);
 
-    do {
-        cout << "Select a mode (Manual 'm' or automatic 'a'):" << endl;
-        cin >> mode;
-        getchar();
-    } while (mode != 'm' && mode != 'a');
+            set_array array_a(a), array_b(b), array_c(c), array_d(d);
+            set_list list_a(a), list_b(b), list_c(c), list_d(d);
+            set_bool_array bool_array_a(a), bool_array_b(b), bool_array_c(c), bool_array_d(d);
+            set_machine_word machine_word_a(a), machine_word_b(b), machine_word_c(c), machine_word_d(d);
 
-    if (mode == 'm') {
-        cout << "Enter the sets as a string consisting of numbers.\n";
-        cout << "A:";
-        scan_set(a);
-        cout << "B:";
-        scan_set(b);
-        cout << "C:";
-        scan_set(c);
-        cout << "D:";
-        scan_set(d);
-    } else {
-        cout << "Enter length of set (0 to 10):\n";
-        cin >> ln;
-        generatorWithFixedLen(a, ln);
-        generatorWithFixedLen(b, ln);
-        generatorWithFixedLen(c, ln);
-        generatorWithFixedLen(d, ln);
+            auto start = chrono::high_resolution_clock::now();
+            (array_a & array_b) | array_c | array_d;
+            auto stop = chrono::high_resolution_clock::now();
+            cout <<chrono::duration_cast<chrono::nanoseconds>(stop - start).count() << "\n";
+            ~array_a;
+            ~array_b;
+            ~array_c;
+            ~array_d;
+
+            start = chrono::high_resolution_clock::now();
+            (list_a & list_b) | list_c | list_d;
+            stop = chrono::high_resolution_clock::now();
+            cout << chrono::duration_cast<chrono::nanoseconds>(stop - start).count() << "\n";
+            ~list_a;
+            ~list_b;
+            ~list_c;
+            ~list_d;
+
+            start = chrono::high_resolution_clock::now();
+            (bool_array_a & bool_array_b) | bool_array_c | bool_array_d;
+            stop = chrono::high_resolution_clock::now();
+            cout << chrono::duration_cast<chrono::nanoseconds>(stop - start).count() << "\n";
+            ~bool_array_a;
+            ~bool_array_b;
+            ~bool_array_c;
+            ~bool_array_d;
+
+            start = chrono::high_resolution_clock::now();
+            (machine_word_a & machine_word_b) | machine_word_c | machine_word_d;
+            stop = chrono::high_resolution_clock::now();
+            cout  << chrono::duration_cast<chrono::nanoseconds>(stop - start).count() << "\n";
+            ~machine_word_a;
+            ~machine_word_b;
+            ~machine_word_c;
+            ~machine_word_d;
+        }
     }
-
-    if (mode == 'a') {
-        cout << "A: " << a << "\n";
-        cout << "B: " << b << "\n";
-        cout << "C: " << c << "\n";
-        cout << "D: " << d << "\n";
-    }
-
-    set_array array_a(a), array_b(b), array_c(c), array_d(d);
-    set_list list_a(a), list_b(b), list_c(c), list_d(d);
-    set_bool_array bool_array_a(a), bool_array_b(b), bool_array_c(c), bool_array_d(d);
-    set_machine_word machine_word_a(a), machine_word_b(b), machine_word_c(c), machine_word_d(d);
-
-    auto start = chrono::high_resolution_clock::now();
-    (array_a & array_b) | array_c | array_d;
-    auto stop = chrono::high_resolution_clock::now();
-    cout << "\nArray result:\t\t\t";
-    ((array_a & array_b) | array_c | array_d).show();
-    cout << " in " << chrono::duration_cast<chrono::nanoseconds>(stop - start).count() << " nanoseconds\n";
-    ~array_a;
-    ~array_b;
-    ~array_c;
-    ~array_d;
-
-    start = chrono::high_resolution_clock::now();
-    (list_a & list_b) | list_c | list_d;
-    stop = chrono::high_resolution_clock::now();
-    cout << "List result:\t\t\t";
-    ((list_a & list_b) | list_c | list_d).show();
-    cout << " in " << chrono::duration_cast<chrono::nanoseconds>(stop - start).count() << " nanoseconds\n";
-    ~list_a;
-    ~list_b;
-    ~list_c;
-    ~list_d;
-
-    start = chrono::high_resolution_clock::now();
-    (bool_array_a & bool_array_b) | bool_array_c | bool_array_d;
-    stop = chrono::high_resolution_clock::now();
-    cout << "Bool array result:\t\t";
-    ((bool_array_a & bool_array_b) | bool_array_c | bool_array_d).show();
-    cout << " in " << chrono::duration_cast<chrono::nanoseconds>(stop - start).count() << " nanoseconds\n";
-    ~bool_array_a;
-    ~bool_array_b;
-    ~bool_array_c;
-    ~bool_array_d;
-
-    start = chrono::high_resolution_clock::now();
-    (machine_word_a & machine_word_b) | machine_word_c | machine_word_d;
-    stop = chrono::high_resolution_clock::now();
-    cout << "Machine word result:\t";
-    ((machine_word_a & machine_word_b) | machine_word_c | machine_word_d).show();
-    cout << " in " << chrono::duration_cast<chrono::nanoseconds>(stop - start).count() << " nanoseconds\n";
-    ~machine_word_a;
-    ~machine_word_b;
-    ~machine_word_c;
-    ~machine_word_d;
-    cout.flush();
-
 }
