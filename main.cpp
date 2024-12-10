5ff993d2e6f25e2ab99b36c7c6b5f07acc33e9aa
ivan <ivan.sharapov.05@yandex.ru>
2024-12-10 13:23:34 +0300

    Переделали выделение двудольной компоненты

--- main.cpp
+++ main.cpp
@@ -5,46 +5,44 @@
 using namespace std;
 
 int main() {
-    int n, m, u, v;
+    int n;
+    int m;
     cin >> n >> m;
+
     vector<vector<int>> g(n);
+    vector<pair<int, int>> edges(m);
+    vector<int> color(n, 0);
+    queue<int> q;
+
     for (int i = 0; i < m; ++i) {
+        int u, v;
         cin >> u >> v;
         u--;
         v--;
         g[u].push_back(v);
         g[v].push_back(u);
+        edges[i] = {u, v};
     }
-    vector<int> color(n, 0);
-    int cnt = 0;
-    vector<vector<pair<int, int>>> spanning_edges;
-    queue<int> q;
+
     for (int i = 0; i < n; ++i) {
         if (!color[i]) {
-            spanning_edges.emplace_back();
             color[i] = 1;
             q.push(i);
             while (!q.empty()) {
-                u = q.front();
+                int u = q.front();
                 q.pop();
-                for (auto w: g[u]) {
+                for (int w: g[u]) {
                     if (!color[w]) {
                         color[w] = color[u] == 1 ? 2 : 1;
-                        spanning_edges[cnt].emplace_back(u, w);
                         q.push(w);
-                    } else if (color[w] != color[u]) {
-                        spanning_edges[cnt].emplace_back(u, w);
                     }
                 }
             }
-            ++cnt;
         }
     }
 
-    cout << "Count of connected components: " << cnt << "\n";
-    for (int i = 0; i < cnt; ++i) {
-        cout << "\nComponent " << i + 1 << ":\n";
-        for (auto x: spanning_edges[i]) {
+    for (auto x: edges) {
+        if (color[x.first] != color[x.second]) {
             cout << x.first + 1 << "-" << x.second + 1 << "\n";
         }
     }
